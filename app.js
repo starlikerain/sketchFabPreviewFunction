aed4c00b27f46b52c2ce190e89a00864ee50826c
starlikerain <pengyaokang@qq.com>
2021-10-17 15:25:27 +0800

    feat(demo): resource

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let scene, camera, renderer
 const init = () => {
     // Scene
     scene = new THREE.Scene()
-    scene.background = new THREE.Color(0x000000)
+    scene.background = new THREE.Color('green')
 
     // Render
-    renderer = new THREE.WebGLRenderer({antialias: true})
+    renderer = new THREE.WebGLRenderer({antialias: true, logarithmicDepthBuffer: true})
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
 
@@ -14,23 +14,36 @@ const init = () => {
     const aspect = window.innerWidth / window.innerHeight
     camera = new THREE.PerspectiveCamera(60, aspect, 0.01, 5000)
     camera.rotation.y = (90/180) * Math.PI
-    camera.position.set(0.5, 0,0)
+    camera.position.set(5300, 2600,0)
+    // camera.position.set(0, 0,0)
 
     // Camera Controller
     const controls = new THREE.OrbitControls(camera, renderer.domElement)
     controls.addEventListener('change', renderer)
 
     // Light
-    let ambientLight = new THREE.AmbientLight(0xaaaaaa,20)
+    let ambientLight = new THREE.AmbientLight("grey",20)
     scene.add(ambientLight)
+
+
 
     // Loader
     let loader = new THREE.GLTFLoader()
-    loader.load("scene.gltf", (result) => {
+    // loader.load("scene2.gltf", (result) => {
+    //     scene.add(result.scene)
+    //     renderer.render(scene, camera)
+    //     animate()
+    // })
+    loader.load("./gothic_building_for_aria_pack_1.0_3D_modal/scene.gltf", (result) => {
         scene.add(result.scene)
         renderer.render(scene, camera)
         animate()
     })
+    // loader.load("./castle_of_loarre_3D_modal/scene.gltf", (result) => {
+    //     scene.add(result.scene)
+    //     renderer.render(scene, camera)
+    //     animate()
+    // })
 }
 
 // Recursive Loop for Render Scene
